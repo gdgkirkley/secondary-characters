0fdd5031a3eed946e26bebbb28262d1aa540f08f
Gabe <gkirkley@artsclub.com>
2019-10-30 12:34:19 -0700

    Attempting just a style change

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,3 @@
-import React from 'react';
 import CMS from 'netlify-cms-app';
-import StyleSheetInjector from './stylesheetinjector';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview';
-
-CMS.registerPreviewStyle('/sc.css');
-
-CMS.registerPreviewTemplate('about', props => (
-  <StyleSheetInjector>
-    <AboutPagePreview {...props} />
-  </StyleSheetInjector>
-));
+import './sc.css';
