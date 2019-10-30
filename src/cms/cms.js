857af2894d522bea9dec134813fb6ccc70423859
Gabe <gkirkley@artsclub.com>
2019-10-30 13:21:36 -0700

    Updating preview style. Attempting another about page template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,16 @@
+import React from 'react';
 import CMS from 'netlify-cms-app';
+import StyleSheetInjector from './stylesheetinjector';
+
+import AboutPagePreview from './preview-templates/AboutPagePreview';
 
 import './sc.css';
+
+CMS.registerPreviewTemplate('about', props => {
+  console.log(props);
+  return (
+    <StyleSheetInjector>
+      <AboutPagePreview {...props} />
+    </StyleSheetInjector>
+  );
+});
