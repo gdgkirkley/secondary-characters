bd361bb862dc5d658505e7769aeb11d07daa822a
Gabe Kirkley <gdgkirkley@gmail.com>
2021-07-02 13:19:07 -0700

    Remove display button links from sections

--- src/templates/contentpage.js
+++ src/templates/contentpage.js
@@ -25,7 +25,6 @@ export const query = graphql`
             sections {
               sectionHead
               content
-              displayButton
             }
           }
           html
--- src/templates/formpage.js
+++ src/templates/formpage.js
@@ -29,7 +29,6 @@ export const query = graphql`
             sections {
               sectionHead
               content
-              displayButton
             }
           }
           html
